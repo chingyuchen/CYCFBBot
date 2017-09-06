fb2e150d9ce7ccd2c20eddc98f709db8e75692cd
Ching-Yu Chen <chingyuc.cyc@gmail.com>
2017-09-05 23:29:25 -0400

    Add customized bot function

--- fbbot.py
+++ fbbot.py
@@ -39,8 +39,14 @@ botid = fbmq.Page(TOKEN).page_id
 app = Flask(__name__)
 
 # object analyzes if the command from the user is valid.
-cmdanalyzer = CmdAnalyzer()
+cmd_analyzer = CmdAnalyzer()
 
+#
+portnum = 5000
+
+#
+invalidmsg = 'Not a valid command. Please retype the command or type /help for'
+' command instructions.'
 ################################################################################
 
 @app.route('/', methods=['POST'])
@@ -58,11 +64,10 @@ def handle_incoming_messages():
         return "ok"
 
     if msg_type is not 'state':
-        if cmdanalyzer.is_command(data): 
-            cmdanalyzer.execute(chat_id, msg_content)
+        if cmd_analyzer.is_command(data): 
+            cmd_analyzer.execute(chat_id, msg_content)
         else:
-            bot.send_text_message(chat_id, 'Not a valid command. Please retype '
-                'the command or type /help for command instructions.')
+            bot.send_text_message(chat_id, invalidmsg)
         
     else:
         pass
@@ -71,14 +76,38 @@ def handle_incoming_messages():
 
 ################################################################################
 
+def set_port(num):
+    #need to add catch exception
+    portnum = num 
+
+def set_invalid_cmd_msg(msg):
+    # need to check string type
+    invalidmsg = msg
+
 def run():
 
     ''' 
     Starts running the bot
     '''
 
-    app.run(debug=True)
+    app.run(port=portnum, debug=True)
+
+def add_cmdpgm(pgmcmd):
+    # check valid pgmname, valid string type
+    # static method in module of cmdanalyzer
+    cmdanalyzer.add_cmdpgm(pgmcmd, Pgm(pgmcmd))
+
+def remove_cmdpgm(pgmcmd):
+    # check valid
+    cmdanalyzer.remove_cmdpgm(pgmcmd)
  
+def add_pgm_state(pgmcmd, statename, check_cmd_function, state_function):
+    # check valid arguments
+    # check if function returns state
+    # should use another function to help user create state function
+    cmdanalyzer.add_pgm_state(pgmcmd, statename, check_cmd_function, \
+            state_function)
+
 #-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
