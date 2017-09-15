167a0cb333119ef571fbefb72e80d04d407f9a5c
Ching-Yu Chen <chingyuc.cyc@gmail.com>
2017-09-15 01:03:20 -0400

    Add send location

--- messenger.py
+++ messenger.py
@@ -1,6 +1,16 @@
 import requests
 import json
 
+key = ""
+try:
+    with open('geocoder_key', 'r') as f:
+        key = f.read().strip()
+        f.close()
+    assert(len(key) != 0)
+except:
+    print("error in accessing geocoder key")
+
+
 try:
     TOKEN = ""
     with open('Token', 'r') as f:
@@ -61,6 +71,36 @@ def send_buttons(user, text, buttons):
 
 #-------------------------------------------------------------------------------
 
+def send_location(user, lat, lon):
+    # check arguments
+    imageurl = "https://maps.googleapis.com/maps/api/staticmap?key=" + key +\
+        "&markers=color:red|label:B|" + str(lat) + "," + str(lon) + "&size=360x360&zoom=13"
+    print(imageurl)
+    itemurl = "http://maps.apple.com/maps?q="+ str(lat) +","+ str(lon) +"&z=16"
+    print(itemurl)
+    data = {
+                "recipient": {"id": user},
+                "message": {
+                    "attachment":{
+                        "type": "template", 
+                        "payload":{
+                            "template_type":"generic",
+                            "elements":{
+                                "element":{
+                                    "title": "Nearest station",
+                                    "image_url":imageurl,
+                                    "item_url":itemurl
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+                                    #"item_utl":itemurl
+    resp = requests.post(urlS + TOKEN, json=data)
+    print(str(resp))
+#-----------------------------------------------------------------------
 def get_page_info():
     resp = requests.get("https://graph.facebook.com/v2.6/me",
                         params={"access_token": TOKEN},
@@ -78,4 +118,12 @@ def get_user_info(user):
 
     info = json.loads(resp.text)
     return info
+
+
+#########################################################################
+
+if __name__ == "__main__":
+    user = "1454392541282560"
+    send_text(user, "hi")
+    send_location(user, 40, 70)
     
